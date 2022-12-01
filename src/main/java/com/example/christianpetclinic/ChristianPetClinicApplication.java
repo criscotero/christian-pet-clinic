08b686640946e576d66d238428db7ade300c6c53
christian.otero <iry7v2jy477ruli2nx7y7zkmuqmjvpnoq6vtv7hthp5avpdq3cyq>
2022-12-01 11:54:17 -0500

    First commit

---
+++ src/main/java/com/example/christianpetclinic/ChristianPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.christianpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristianPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChristianPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/christianpetclinic/ChristianPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.christianpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristianPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
